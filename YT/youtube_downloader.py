2cfac56f79374c480ca6433d6eca9d169a200b86
Nomez <p.300@o2.pl>
2024-12-17 19:37:19 +0100

    add requirements.txt

--- YT/youtube_downloader.py
+++ YT/youtube_downloader.py
@@ -19,8 +19,7 @@ def download_youtube(video_url, path):
 # Pobranie ścieżki zapisu z pliku .env
 load_dotenv()  # Wczytanie zmiennych z pliku .env
 output_path = getenv('DOWNLOAD_PATH')  # Zmienna środowiskowa DOWNLOAD_PATH
-print(f"Ścieżka zapisu: {output_path}")  # Dodaj ten wydruk, aby sprawdzić, co jest odczytywane
 if output_path:
-    download_youtube('https://www.youtube.com/watch?v=OabDZn1KYjc', output_path)
+    download_youtube('https://www.youtube.com/watch?v=aSAYjtZOdrE', output_path)
 else:
     print("Ścieżka zapisu nie została zdefiniowana w pliku .env.")
